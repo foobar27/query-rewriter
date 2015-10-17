f5a66e7ab05a79d662057566daefebbb56343ea1
Sebastien Wagener <sebastien.wagener@gmail.com>
2015-10-17 21:09:21 +0200

    program execution

---
+++ src/query_rewriter/category.clj
@@ -0,0 +1,123 @@
+(ns query-rewriter.category
+  (:require [query-rewriter.rewrite :as rewrite :refer [program-with-symbols rule run-program]]))
+
+;; TODO defn category
+
+(defn object [& {:keys [category value]}]
+  (program-with-symbols [category value]
+    [::object category value]))
+
+(defn morphism [& {:keys [category source target value]}]
+  (program-with-symbols [category source target value]
+    ((object :category category) source)
+    ((object :category category) target)
+    [::morphism category source target value]))
+
+(defn identity [& {:keys [category object]}]
+  (program-with-symbols [category object]
+    (morphism :source object :target object :category category :value ::identity)))
+
+(defn compose [m1 m2]
+  (program-with-symbols [m1 m2 :local o1 o2 o3]
+    ((morphism :source o1 :target o2) m1)
+    ((morphism :source o2 :target o3) m2)
+    (morphism :source o1 :target o3 :value [::compose m1 m2])))
+
+(defn left-identity-rule []
+  (program-with-symbols [:local f]
+    (rule (compose (identity) f)
+          f)))
+
+(defn right-identity-rule []
+  (program-with-symbols [:local f]
+    (rule (compose f (identity))
+          f)))
+
+
+
+
+
+;; (defrule associativity-rule-1 [m1 m2 m3]
+;;   (compose m3 (compose m2 m1))
+;;   -> (compose (compose m3 m2) m1))
+
+;; (defrule associativity-rule-2 [m1 m2 m3]
+;;   (compose (compose m3 m2) m1)
+;;   -> (compose m3 (compose m2 m1)))
+
+
+;;
+;; products
+;;
+
+;; TODO somehow store the constraint that this category has products
+;; TODO require project-left & co to be defined dependently on the category
+;; TODO split, project-left, project-right
+
+;; (defrule product-cancellation-left-rule [f g]
+;;   (compose (project-left) (split f g))
+;;   -> f)
+
+;; (defrule product-cancellation-right-rule [f g]
+;;   (compose (project-right) (split f g))
+;;   -> g)
+
+;; (defpattern morphism-product [f g]
+;;   (split (compose f (project-left))
+;;          (compose g (project-right))))
+
+;;
+;; sums
+;;
+
+;; (defrule sum-cancellation-left-rule [f g]
+;;   (compose (junc f g) (inject-left))
+;;   -> f)
+
+;; (defrule sum-cancellation-right-rule [f g]
+;;   (compose (junc f g) (inject-right))
+;;   -> g)
+
+;; (defpattern morphism-sum [f g]
+;;   (split (compose (inject-left) f)
+;;          (compose (inject-right) g)))
+
+;;
+;; monads
+;;
+
+;; TODO
+
+;;
+;; unsorted
+;;
+
+;; [:morphism
+;;  C
+;;  [A B]
+;;  [:compose
+;;   [:morphism
+;;    C
+;;    [B B]
+;;    'identity]
+;;   [:morphism
+;;    C
+;;    [A B]
+;;    f]]]
+;; -> [:morphism C [A B] f]
+
+;; TODO functor
+;; TODO bi-functor
+;; TODO initiality
+
+(comment
+  (require '[clojure.pprint :refer [pprint]])
+  (pprint ((compose 'm1 'm2) 'ret))
+
+  (pprint (run-program (left-identity-rule))))
+
+
+;; TODO make dir-local: http://www.gnu.org/software/emacs/manual/html_node/emacs/Directory-Variables.html
+;; Local Variables:
+;; eval: (put-clojure-indent 'program-with-symbols 1)
+;; End:
---
+++ src/query_rewriter/rewrite.clj
@@ -0,0 +1,88 @@
+(ns query-rewriter.rewrite
+  (:require [clojure.core.logic :as logic]))
+
+;; # Overview
+
+;; This namespace defines a term-rewriting system with the following features:
+;; * statically typed
+;; * automatic type-inference (via core.logic)
+;; * configurable term rewriting strategies (via stratege)
+
+;; The term-rewriting system consists of constructors and rules, which operate
+;; on nested constructors. Both define sets of rules to be used by core.logic
+;; for the type inference, and for the pattern matching.
+
+;; TODO put into context:
+;; At every call site of a function, the non-provided symbols need to be regenerated.
+
+
+;; # Constructors
+
+;; A constructor can take several logic symbols as arguments, required ones and optional ones.
+;; * The first argument is the symbol which will be mapped to the return value.
+;; * The required arguments are positional arguments and must be provided
+;;   immediately after the return symbol.
+;; * The optional arguments are named arguments and must be provided after the
+;;   required ones.
+
+
+(defmacro with-gensyms [symbols & body]
+  (if (seq symbols)
+    `(let [~@(apply concat (for [symbol symbols]
+                             `[~symbol (gensym ~(str symbol))]))]
+       ~@body)
+    `(do
+       ~body)))
+
+(defn- split-args-and-locals [symbols]
+  (let [[args [_ & locals]] (split-with #(not (= % :local)) symbols)]
+    [args locals]))
+
+(defn- binding->program [variable]
+  (fn [ret]
+    (cond
+      (nil? variable)
+      []
+
+      (symbol? variable)
+      `[(logic/== ~ret ~variable)]
+
+      ;; function call
+      (fn? variable)
+      (variable ret)
+
+      ;; constant
+      true
+      `[(logic/== ~ret ~variable)])))
+
+(defmacro program-with-symbols [symbols & body]
+  (let [[args locals] (split-args-and-locals symbols)
+        statements (butlast body)
+        return-statement (last body)
+        ret-symbol (gensym "ret")
+        args-symbols (into {}
+                           (for [symbol args]
+                             [symbol (gensym (str symbol "-argument"))]))]
+    `(fn [~ret-symbol]
+       (let [~@(apply concat (for [[s gs] args-symbols]
+                               [gs s]))]
+         (with-gensyms [~@(concat args locals)]
+           `[(clojure.core.logic/fresh [~~@(concat args locals)]
+               ~@(apply concat (for [[s# gs#] ~args-symbols]
+                                 ((#'binding->program gs#) s#)))
+               ~@~@statements
+               ~@((#'binding->program ~return-statement) ~ret-symbol))])))))
+
+(defn run-program [program]
+  (let [ret (gensym "ret")]
+    (eval
+     `(do
+        (require 'clojure.core.logic)
+        (clojure.core.logic/run* [~ret]
+         ~@(program ret))))))
+
+;; TODO rule macro with syntactic sugar?
+(defn rule [left right]
+  (program-with-symbols [left right]
+    [::rule left right]))
+
